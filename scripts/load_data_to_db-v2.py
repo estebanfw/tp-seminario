4f8bd2f90a3d32ecf4d71b1338461f6b3b18b67f
estebanfw <estebanfwenger@gmail.com>
2021-08-27 23:54:50 -0300

    script to query api and inster into db

---
+++ scripts/load_data_to_db-v2.py
@@ -0,0 +1,41 @@
+import requests, json
+import psycopg2
+import datetime as dt
+
+URL="https://api.coindesk.com/v1/bpi/currentprice.json"
+r = requests.get(url=URL)
+data=r.json()
+print("Request done to {} at ".format(URL,dt.datetime.now()))
+print("-------------------------------------------------------------------------")
+print("Request Output")
+print(r.json())
+
+time=data.get("time").get("updatedISO")
+cryptocoin=data.get("chartName")
+usdprice=data.get("bpi").get("USD").get("rate_float")
+
+try:
+    connection = psycopg2.connect(user="airflow",
+                                  password="airflow",
+                                  host="192.168.1.8",
+                                  port="5435",
+                                  database="airflow")
+    cursor = connection.cursor()
+
+    postgres_insert_query = """ INSERT INTO btc (datetime, coin, price) VALUES (%s,%s,%s)"""
+    record_to_insert = (time, cryptocoin, usdprice)
+    cursor.execute(postgres_insert_query, record_to_insert)
+
+    connection.commit()
+    count = cursor.rowcount
+    print(count, "Record inserted successfully into btc table")
+
+except (Exception, psycopg2.Error) as error:
+    print("Failed to insert record into btc table", error)
+
+finally:
+    # closing database connection.
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
